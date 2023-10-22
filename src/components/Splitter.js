a128ae8e1aa08351f33082666857533077dff1b8
Muqtadirat <muqtadir.my@gmail.com>
2023-10-22 16:58:33 +0100

    Created splitter

---
+++ src/components/Splitter.js
@@ -0,0 +1,63 @@
+
+function Splitter() {
+    return (
+      <div className="Splitter">
+        <div class="overlap-group-wrapper">
+          <div class="overlap-group">
+            <div class="how-i-create-amazing">
+              How I<br />
+              create amazing
+              <br />
+              products
+            </div>
+            <div class="frame">
+              <div class="text-wrapper">Product Definition</div>
+            </div>
+            <div class="div-wrapper">
+              <div class="text-wrapper">Developer Communication</div>
+            </div>
+            <div class="div">
+              <div class="text-wrapper">User Research</div>
+            </div>
+            <div class="sketching-wrapper">
+              <div class="text-wrapper">Sketching &amp; Wireframing</div>
+            </div>
+            <div class="frame-2">
+              <div class="text-wrapper">User Testing</div>
+            </div>
+          </div>
+        </div>
+        <div class="overlap-group-wrapper">
+          <div class="overlap">
+            <div class="overlap-2">
+              <div class="overlap-3">
+                <div class="how-i-create-amazing-2">
+                  How I<br />
+                  create amazing
+                  <br />
+                  products
+                </div>
+                <div class="frame-3">
+                  <div class="text-wrapper-2">User Research</div>
+                </div>
+                <div class="frame-4">
+                  <div class="text-wrapper-2">Sketching &amp; Wireframing</div>
+                </div>
+              </div>
+              <div class="frame-5">
+                <div class="text-wrapper-2">Product Definition</div>
+              </div>
+              <div class="frame-6">
+                <div class="text-wrapper-2">Developer Communication</div>
+              </div>
+            </div>
+            <div class="frame-7">
+              <div class="text-wrapper-2">User Testing</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Splitter;
