4ecc938039377c1219a550cee4b7f3c2c289dced
Muqtadirat <muqtadir.my@gmail.com>
2023-10-26 03:45:11 +0100

    Updated splitter

--- src/components/Splitter.js
+++ src/components/Splitter.js
@@ -1,63 +1,86 @@
+import React from "react";
 
 function Splitter() {
-    return (
-      <div className="Splitter">
-        <div class="overlap-group-wrapper">
-          <div class="overlap-group">
-            <div class="how-i-create-amazing">
+  return (
+    <div className="Splitter hidden md:block w-[714px] h-[270px]">
+      {/* First section */}
+
+      <div className="wrapper font-largescreen-heading text-greyscale-40 h-[270px] w-[714px] ">
+        <div className="how-i-create-amazing absolute font-largescreen-body top-[1233px] left-[390px] text-center text-[80px] font-black">
+          How I<br />
+          create amazing
+          <br />
+          products
+        </div>
+        <div className="dormant inline-flex items-center justify-center gap-[10px] px-[40px] py-[16px] absolute top-[1266px] left-[825px] bg-greyscale-20 rounded-[4px] rotate-[4.696deg]">
+          <div className="text-wrapper relative w-fit mt-[-1.00px]">
+            Product Definition
+          </div>
+        </div>
+        <div className="dormant inline-flex items-center justify-center gap-[10px] px-[40px] py-[16px] absolute top-[1544px] left-[825px] bg-greyscale-20 rounded-[4px] overflow-hidden rotate-[-5.87deg]">
+          <div className="text-wrapper relative w-fit mt-[-1.00px]">
+            Developer Communication
+          </div>
+        </div>
+        <div className="dormant inline-flex items-center justify-center gap-[10px] px-[40px] py-[16px] absolute top-[1276px] left-[447px] bg-greyscale-20 rounded-[4px] rotate-[-11.03deg]">
+          <div className="text-wrapper relative w-fit mt-[-1.00px]">
+            User Research
+          </div>
+        </div>
+        <div className="dormant inline-flex items-center justify-center gap-[10px] px-[40px] py-[16px] absolute top-[1509px] left-[442px] bg-greyscale-20 rounded-[4px] rotate-[4.03deg]">
+          <div className="text-wrapper relative w-fit mt-[-1.00px]">
+            Sketching &amp; Wireframing
+          </div>
+        </div>
+        <div className="dormant inline-flex items-center justify-center gap-[10px] px-[40px] py-[16px] absolute top-[1387px] left-[938px] bg-greyscale-20 rounded-[4px] rotate-[4.40deg]">
+          <div className="text-wrapper relative w-fit mt-[-1.00px]">
+            User Testing
+          </div>
+        </div>
+      </div>
+
+      {/* Second section
+      <div className="overlap-group-wrapper font-largescreen-largedisplay text-center text-blue-400">
+        <div className="overlap absolute w-[873px] h-[450px] top-0 left-0">
+          <div className="overlap-2 absolute w-[752px] h-[328px] top-[71px] left-0 ">
+            <div className="how-i-create-amazing-2 absolute top-[29px] left-[199px] ">
               How I<br />
               create amazing
               <br />
               products
             </div>
-            <div class="frame">
-              <div class="text-wrapper">Product Definition</div>
-            </div>
-            <div class="div-wrapper">
-              <div class="text-wrapper">Developer Communication</div>
-            </div>
-            <div class="div">
-              <div class="text-wrapper">User Research</div>
-            </div>
-            <div class="sketching-wrapper">
-              <div class="text-wrapper">Sketching &amp; Wireframing</div>
-            </div>
-            <div class="frame-2">
-              <div class="text-wrapper">User Testing</div>
+            <div className="overlap-3 text-greyscale-100">
+              <div className="frame-3 inline-flex items-center justify-center gap-[10px] px-[40px] py-[16px] absolute top-[29px] left-[96px] rounded-[4px] rotate-[-18.31deg] bg-base-colorssecondary-blue">
+                <div className="text-wrapper-2 relative w-fit mt-[-1.00px]">
+                  User Research
+                </div>
+              </div>
+              <div className="frame-4 inline-flex items-center justify-center gap-[10px] px-[40px] py-[16px] absolute top-[243px] left-[3px] rounded-[4px] rotate-[-12.38deg] bg-base-colorssecondary-blue">
+                <div className="text-wrapper-2 relative w-fit mt-[-1.00px]">
+                  Sketching &amp; Wireframing
+                </div>
+              </div>
+              <div className="frame-5 inline-flex items-center justify-center gap-[10px] px-[40px] py-[16px] absolute top-[41px] left-[648px] rounded-[4px] rotate-[22.93deg] bg-base-colorssecondary-blue">
+                <div className="text-wrapper-2 relative w-fit mt-[-1.00px]">
+                  Product Definition
+                </div>
+              </div>
+              <div className="frame-6 inline-flex items-center justify-center gap-[10px] px-[40px] py-[16px] absolute top-[386px] left-[530px] rounded-[4px] overflow-hidden rotate-[3.19deg] bg-base-colorssecondary-blue">
+                <div className="text-wrapper-2 relative w-fit mt-[-1.00px]">
+                  Developer Communication
+                </div>
+              </div>
+              <div className="frame-7 inline-flex items-center justify-center gap-[10px] px-[40px] py-[16px] top-[386px] left-[530px] rounded-[4px] rotate-[-4.02deg] bg-base-colorssecondary-blue">
+                <div className="text-wrapper-2 relative w-fit mt-[-1.00px]">
+                  User Testing
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div class="overlap-group-wrapper">
-          <div class="overlap">
-            <div class="overlap-2">
-              <div class="overlap-3">
-                <div class="how-i-create-amazing-2">
-                  How I<br />
-                  create amazing
-                  <br />
-                  products
-                </div>
-                <div class="frame-3">
-                  <div class="text-wrapper-2">User Research</div>
-                </div>
-                <div class="frame-4">
-                  <div class="text-wrapper-2">Sketching &amp; Wireframing</div>
-                </div>
-              </div>
-              <div class="frame-5">
-                <div class="text-wrapper-2">Product Definition</div>
-              </div>
-              <div class="frame-6">
-                <div class="text-wrapper-2">Developer Communication</div>
-              </div>
-            </div>
-            <div class="frame-7">
-              <div class="text-wrapper-2">User Testing</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+      </div> */}
+    </div>
+  );
 }
 
 export default Splitter;
