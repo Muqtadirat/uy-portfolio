06028e3824c498a5341ac5b8ae835d8118b947f6
Muqtadirat <muqtadir.my@gmail.com>
2023-11-01 19:54:04 +0100

    Created component for Aufera project

---
+++ src/components/Aufera.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Aufera() {
+  return (
+    <div>
+      <div>
+        <button>
+          <Link to="/">Back </Link>{" "}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Aufera;
