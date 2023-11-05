00a1ba09ca41dd2e145c2921f27f039ebce6180f
Muqtadirat <muqtadir.my@gmail.com>
2023-11-05 12:59:30 +0100

    Imported footer component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import CustomRoutes from "./components/CustomRoutes";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App container">
       <NavBar />
       <CustomRoutes />
-
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
