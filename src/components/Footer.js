ca00d5397a9dd53f2aa6babfc02c36bc22beb2f5
Muqtadirat <muqtadir.my@gmail.com>
2023-11-05 12:58:42 +0100

    Updated footer
    Added the footer icons and links to their respective sites

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,47 +16,49 @@ function Footer() {
   };
 
   return (
-    <div className="Footer flex bg-greyscale-20 absolute w-[1368px]">
-      <img src={logo} className="logo" alt="UY-logo" style={logoStyle} />
-      <div className="socials flex">
-        <div className="twitter">
-          <a
-            href="https://twitter.com/UthmanYussuff_O"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={twitterLogo} alt="twitter" style={socialLogo} />
-          </a>
-        </div>
+    <div className="Footer bg-base-colorssurface absolute w-full border-t">
+      <div className="w-[1368px] flex justify-between mx-auto items-center py-5">
+        <img src={logo} className="logo" alt="UY-logo" style={logoStyle} />
+        <div className="socials flex gap-6 ">
+          <div className="twitter">
+            <a
+              href="https://twitter.com/UthmanYussuff_O"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={twitterLogo} alt="twitter" style={socialLogo} />
+            </a>
+          </div>
 
-        <div className="linkedIn">
-          <a
-            href="https://www.linkedin.com/in/uthmanyussuff/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={linkedinLogo} alt="twitter" style={socialLogo} />
-          </a>
-        </div>
+          <div className="linkedIn">
+            <a
+              href="https://www.linkedin.com/in/uthmanyussuff/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={linkedinLogo} alt="twitter" style={socialLogo} />
+            </a>
+          </div>
 
-        <div className="dribble">
-          <a
-            href="https://dribbble.com/UthmanYussuff"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={dribbleLogo} alt="twitter" style={socialLogo} />
-          </a>
-        </div>
+          <div className="dribble">
+            <a
+              href="https://dribbble.com/UthmanYussuff"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={dribbleLogo} alt="twitter" style={socialLogo} />
+            </a>
+          </div>
 
-        <div className="medium">
-          <a
-            href="https://medium.com/@uthmanyussuff"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={mediumLogo} alt="twitter" style={socialLogo} />
-          </a>
+          <div className="medium">
+            <a
+              href="https://medium.com/@uthmanyussuff"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={mediumLogo} alt="twitter" style={socialLogo} />
+            </a>
+          </div>
         </div>
       </div>
     </div>
