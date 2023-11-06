44bdc846963130df1aa458a95af0836200ebdf2c
Muqtadirat <muqtadir.my@gmail.com>
2023-11-06 16:19:10 +0100

    Created userPersona comp
    Component for the user info section on the Aufera page

---
+++ src/components/auferaComp/UserPersona.jsx
@@ -0,0 +1,360 @@
+import azeezCard from "./images/Azeez-card.png";
+import nnekaCard from "./images/Nneka-card.png";
+import helenCard from "./images/Helen-card.png";
+import yemiCard from "./images/Yemi-card.png";
+
+function FaceCard({ imgSrc }) {
+  const imgStyle = {
+    width: "350px",
+    height: "361px",
+  };
+
+  return (
+    <div>
+      <img src={imgSrc} alt="User Face Card" style={imgStyle} />
+    </div>
+  );
+}
+
+function UserPersona() {
+  return (
+    <div className="userPersona bg-greyscale-100">
+      <div className="personaWrapper mx-[72px] py-[80px] text-greyscale-40">
+        <h2 className="text-greyscale-10 text-[48px] flex mb-[40px] font-largescreen-largedisplay">
+          User Personas
+        </h2>
+
+        <div className="userCard bg-greyscale-97 border rounded-[4px] border-greyscale-90 gap-[10px] mb-[40px]">
+          <div className="azeezCard mx-[72px] my-[56px] flex gap-[70px]">
+            <div className="flex flex-col items-start gap-[24px] text-[18px]">
+              <FaceCard imgSrc={azeezCard} />
+              <p className="text-greyscale-20 font-bold text-[24px]">
+                Azeez Balogun
+              </p>
+              <div className="gap-[16px] flex flex-col items-start">
+                <p>28 years</p>
+                <p>Ikeja, Lagos</p>
+                <p>Student</p>
+              </div>
+            </div>
+            {""}
+            <div className="userDetails flex flex-col gap-[56px] leading-[24px]">
+              <div className="wrapper flex gap-[32px] text-[18px]">
+                <div className="bio w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Bio
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+
+                <div className="frustration w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Frustrations
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+                {""}
+              </div>
+              {""}
+              <div className="wrapper flex gap-[32px] text-[18px]">
+                <div className="motivation w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Motivations
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+
+                <div className="goal w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Goals
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+                {""}
+              </div>
+            </div>
+          </div>
+          {""}
+        </div>
+        {/* Second card */}
+
+        <div className="userCard bg-greyscale-97 border rounded-[4px] border-greyscale-90 gap-[10px] mb-[40px]">
+          <div className="nnenkaCard mx-[72px] my-[56px] flex gap-[70px]">
+            <div className="flex flex-col items-start gap-[24px] text-[18px]">
+              <FaceCard imgSrc={nnekaCard} />
+              <p className="text-greyscale-20 font-bold text-[24px]">
+                Ayomikun Nneka
+              </p>
+              <div className="gap-[16px] flex flex-col items-start">
+                <p>36 years</p>
+                <p>Ikeja, Lagos</p>
+                <p>IT Professional</p>
+              </div>
+            </div>
+            {""}
+            <div className="userDetails flex flex-col gap-[56px] leading-[24px]">
+              <div className="wrapper flex gap-[32px] text-[18px]">
+                <div className="bio w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Bio
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+
+                <div className="frustration w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Frustrations
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+                {""}
+              </div>
+              {""}
+              <div className="wrapper flex gap-[32px] text-[18px]">
+                <div className="motivation w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Motivations
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+
+                <div className="goal w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Goals
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+                {""}
+              </div>
+            </div>
+          </div>
+          {""}
+        </div>
+
+        {/* Third card */}
+
+        <div className="userCard bg-greyscale-97 border rounded-[4px] border-greyscale-90 gap-[10px] mb-[40px]">
+          <div className="helenCard mx-[72px] my-[56px] flex gap-[70px]">
+            <div className="flex flex-col items-start gap-[24px] text-[18px]">
+              <FaceCard imgSrc={helenCard} />
+              <p className="text-greyscale-20 font-bold text-[24px]">
+                Rashidat Helen
+              </p>
+              <div className="gap-[16px] flex flex-col items-start">
+                <p>24 years</p>
+                <p>Ikeja, Lagos</p>
+                <p>Operation Specialist</p>
+              </div>
+            </div>
+            {""}
+            <div className="userDetails flex flex-col gap-[56px] leading-[24px]">
+              <div className="wrapper flex gap-[32px] text-[18px]">
+                <div className="bio w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Bio
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+
+                <div className="frustration w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Frustrations
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+                {""}
+              </div>
+              {""}
+              <div className="wrapper flex gap-[32px] text-[18px]">
+                <div className="motivation w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Motivations
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+
+                <div className="goal w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Goals
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+                {""}
+              </div>
+            </div>
+          </div>
+          {""}
+        </div>
+
+        {/* Fourth card */}
+
+        <div className="userCard bg-greyscale-97 border rounded-[4px] border-greyscale-90 gap-[10px] gap-x-[40px]">
+          <div className="yemiCard mx-[72px] my-[56px] flex gap-[70px]">
+            <div className="flex flex-col items-start gap-[24px] text-[18px]">
+              <FaceCard imgSrc={yemiCard} />
+              <p className="text-greyscale-20 font-bold text-[24px]">
+                Yemi Hussain
+              </p>
+              <div className="gap-[16px] flex flex-col items-start">
+                <p>50 years</p>
+                <p>Ikeja, Lagos</p>
+                <p>Design Lead</p>
+              </div>
+            </div>
+            {""}
+            <div className="userDetails flex flex-col gap-[56px] leading-[24px]">
+              <div className="wrapper flex gap-[32px] text-[18px]">
+                <div className="bio w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Bio
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+
+                <div className="frustration w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Frustrations
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+                {""}
+              </div>
+              {""}
+              <div className="wrapper flex gap-[32px] text-[18px]">
+                <div className="motivation w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Motivations
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+
+                <div className="goal w-[367px] h-[232px] flex flex-col text-left">
+                  <p className="pb-[16px] font-bold text-[24px] font-largescreen-caption text-greyscale-20">
+                    Goals
+                  </p>
+                  <p>
+                    Phasellus in commodo purus. Nulla porta fringilla
+                    vestibulum. Cras felis mauris, gravida sed egestas vitae,
+                    sollicitudin vel est. Nullam sodales dapibus metus, eu
+                    suscipit urna interdum vel. Sed quis elementum lectus, id
+                    tristique leo. Phasellus tempus tincidunt dui. Mauris dictum
+                    accumsan elit elementum condimentum.
+                  </p>
+                </div>
+                {""}
+              </div>
+            </div>
+          </div>
+          {""}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserPersona;
