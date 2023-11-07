2c311df380ade319197ef1e5b61d6910c49a9fc6
Muqtadirat <muqtadir.my@gmail.com>
2023-11-07 01:59:25 +0100

    Created wireframe section

---
+++ src/components/auferaComp/Frames.jsx
@@ -0,0 +1,26 @@
+import frame from "./images/frames.png"
+
+function Frames() {
+  return (
+    <div className="Frames">
+      <div className="architecture flex my-[88px] text-left gap-[32px]">
+        <p className="text-greyscale-40 w-[668px] font-bold text-[24px] font-largescreen-caption leading-[24px]">
+          Wireframes
+        </p>
+        <div className="text-wrapper w-[668px] text-[18px] leading-[24px]">
+          <p>
+            Wireframing the interface structure was done to ease communication
+            with stakeholders and serve for proof of concept for A/B testing and
+            desirability studies with users.
+          </p> <br />
+          <p>
+            I have shown mid-high fidelity views of some of the screens below.
+          </p>
+        </div>
+          </div>
+          <img src={frame} alt="Wireframes" />
+    </div>
+  );
+}
+
+export default Frames;
