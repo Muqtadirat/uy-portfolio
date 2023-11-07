158138d1cf3d2146b0461e26e68ceaa923b6f8dd
Muqtadirat <muqtadir.my@gmail.com>
2023-11-07 01:58:22 +0100

    Added organizational chart

---
+++ src/components/auferaComp/Chart.jsx
@@ -0,0 +1,13 @@
+import chart from "./images/chart.png";
+
+function Chart() {
+  return (
+    <div className="Chart">
+      <img src={chart} alt="Organizational chart" />
+    </div>
+  );
+}
+
+// Should it be hardcoded ?
+
+export default Chart;
