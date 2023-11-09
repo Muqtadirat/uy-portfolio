d35b1c7abf2d612c5a7f3e2740f0f7d6a3cb76bd
Muqtadirat <muqtadir.my@gmail.com>
2023-11-09 12:34:48 +0100

    Imported Aufera sub components into main component

--- src/components/auferaComp/Aufera.js
+++ src/components/auferaComp/Aufera.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import backArrow from "./images/back-arrow.svg";
+import outArrow from "./images/outward-arrow.svg"
 import CaseStudy from "./CaseStudy";
 import UserPersona from "./UserPersona";
 import Wireframe from "./Wireframe";
+import FinalProduct from "./FinalProduct";
 
 function Aufera() {
   return (
-    <div className="Aufera w-full h-[17372px] bg-base-colorssurface font-largescreen-body text-white">
+    <div className="Aufera w-full h-auto bg-base-colorssurface font-largescreen-body text-white">
       <button className="flex ml-[72px] pt-[135px]">
         <Link to="/" className=" inline-flex gap-[4px]">
           {" "}
@@ -16,6 +18,20 @@ function Aufera() {
       <CaseStudy />
       <UserPersona />
       <Wireframe />
+      <FinalProduct />
+      <div className="w-[216px] pt-[90px] pb-[80px] mx-auto leading-[24px]">
+        <p>Open Project</p>
+        <p>
+          <a
+            href="http://www.aufera.com"
+            target="_blank"
+            rel="noreferrer"
+            className="inline-flex text-[24px] font-bold mt-[16px]"
+          >
+            www.aufera.com <img src={outArrow} alt="Outward arrow" />{" "}
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
