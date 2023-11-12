1ef6ed8d5645520200709af6fa57454e3f17fdd0
Muqtadirat <muqtadir.my@gmail.com>
2023-11-12 15:43:00 +0100

    Updated Acomart main component

--- src/components/acomartComp/Acomart.jsx
+++ src/components/acomartComp/Acomart.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
 import backArrow from "./images/back-arrow.svg";
+import outArrow from "./images/outward-arrow.svg"
 import CaseStudy from "./CaseStudy";
 import WireFrame from "./WireFrame";
+import FinalProduct from "./FinalProduct";
 
 function Acomart() {
   return (
@@ -14,6 +16,21 @@ function Acomart() {
 
       <CaseStudy />
       <WireFrame />
+      <FinalProduct />
+
+      <div className="w-[216px] pt-[90px] pb-[80px] mx-auto leading-[24px]">
+        <p>Open Project</p>
+        <p>
+          <a
+            href="http://www.aufera.com"
+            target="_blank"
+            rel="noreferrer"
+            className="inline-flex text-[24px] font-bold mt-[16px]"
+          >
+            www.aufera.com <img src={outArrow} alt="Outward arrow" />{" "}
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
