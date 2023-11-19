b709940a9a0041b6ab4d4d0255edd5b109d7ca50
Muqtadirat <muqtadir.my@gmail.com>
2023-11-19 11:47:13 +0100

    Created Mecho Website main and sub components

---
+++ src/components/mechoWebComp/MechoWebsite.jsx
@@ -0,0 +1,9 @@
+
+import backArrow from "./mechoWeb images/back-arrow.svg"
+import outArrow from "./mechoWeb images/outward-arrow.svg"
+
+function MechoWebsite() {
+    
+}
+
+export default MechoWebsite;
