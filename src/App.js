31672073507aa363115348a2720e105e79efa993
Muqtadirat <muqtadir.my@gmail.com>
2024-01-28 23:36:08 +0100

    fix: scroll

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import CustomRoutes from "./components/CustomRoutes";
 import Footer from "./components/Footer";
+import Scroll from "./components/Scroll";
 
 function App() {
   return (
     <div className="App container">
+      <Scroll />
       <NavBar />
       <CustomRoutes />
       <Footer />
